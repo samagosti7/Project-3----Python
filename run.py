a55313f2d8bc5f1c2bb95db9b0958571839f98c3
samagosti7 <sagosti7@gmail.com>
2022-04-05 13:30:17 +0000

    Create skeleton of board, and converter from numbers to letters

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Initial Comment
+
+PLAYER_BOARD = [[" "]*7 for x in range(7)]
+GUESS_BOARD = [[" "]*7 for x in range(7)]
+
+print(PLAYER_BOARD)
+letters_to_numbers = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6}
+
+def board():
+    pass
+
+def create_ships():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_hit_ships():
+    pass
+
+
