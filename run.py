e3d670c95e01c2585e53e700b9b16313aa4e05ed
samagosti7 <sagosti7@gmail.com>
2022-04-13 16:47:37 +0000

    Finalize Game logic

--- run.py
+++ run.py
@@ -1,38 +1,67 @@
 # Initial Comment
-
-print("Hello")
+# X for placed battleship and hit battleship
+# ' ' for available
+# "-" for miss
 from random import randint
 
-PLAYER_BOARD = [[" "]*7 for x in range(7)]
+print("Hello")
+COMPUTER_BOARD = [[" "]*7 for x in range(7)]
 GUESS_BOARD = [[" "]*7 for x in range(7)]
 
 
-letters_to_numbers = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6}
+letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6}
 
 
 def print_board(board):
-    print("A B C D E F G")
-    print("-------------")
+    print("  A B C D E F G")
+    print("  -------------")
     row_number = 1
     for row in board:
-        print("|%d|%s|" % (row_number, "|".join(row)))
+        print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
 
-def create_ships():
+def create_ships(board):
     for ship in range(4):
         ship_row, ship_column = randint(0, 6), randint(0, 6)
-        
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = randint(0, 6), randint(0, 6)
+        board[ship_row][ship_column] = "X"
 
 def get_ship_location():
-    pass
+    row = input("Enter ship row between 1 and 7:  ")
+    column = input("Enter ship column between A-G:  ").upper()
+    return int(row)-1, letters_to_numbers[column]
 
-def count_hit_ships():
-    pass
+def count_hit_ships(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == "X":
+                count += 1
+    return count
 
-create_ships()
-turns=10
-# while turns>0:
 
-
-print("Hello")
+create_ships(COMPUTER_BOARD)
+turns = 17
+print_board(COMPUTER_BOARD)
+print("Ahoy admiral, prepare for war! We have 18 shots for the heavy artillery and reconnaissance tells us there are 4 enemy ships lurking in the area. It's up to you to find them!")
+while turns > 0:
+    print_board(GUESS_BOARD)
+    row, column = get_ship_location()
+    if GUESS_BOARD[row][column] == "-":
+        print("Uh...you already guessed that. Are you ok admiral?")
+    elif COMPUTER_BOARD[row][column] == "X":
+        print("A hit! Well shot, sir, ship sunk.")
+        GUESS_BOARD[row][column] = "X"
+        turns -= 1
+    else:
+        print("No luck sir, shot missed.")
+        GUESS_BOARD[row][column] = "-"
+        turns -= 1
+    if count_hit_ships(GUESS_BOARD) == 4:
+        print("That's the last of them sir, we've done it! All battleships sunk, we've won!")
+        break
+    print(f"You have {turns} turns left.")
+    if turns == 0:
+        print("We're out of time. We ran out of turns, so the enemy has escaped with their remaining vessels.")
