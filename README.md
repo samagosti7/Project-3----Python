4a32c9331e0f72dc22b9597535aa0ccbc92d9247
samagosti7 <sagosti7@gmail.com>
2022-04-17 03:11:18 +0000

    Shorten lines in main game, begin readme

--- README.md
+++ README.md
@@ -1,31 +1,30 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# Battleship
 
-Welcome samagosti7,
+Intro Message
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
+<img src="images/responsive.png" alt="image of app on different sized screens">
 
-## Reminders
+[Click here to go to the live website!](https://hang-the-guy.herokuapp.com/) 
 
-* Your code must be placed in the `run.py` file
-* Your dependencies must be placed in the `requirements.txt` file
-* Do not edit any of the other files or your code may not deploy properly
+## Table of contents 
 
-## Creating the Heroku app
-
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
-
-1. `heroku/python`
-2. `heroku/nodejs`
-
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
-
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
-
-Connect your GitHub repository and deploy as normal.
-
-## Constraints
-
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
-
------
-Happy coding!
+1. [Plans and structure](#plans-and-structure)
+    - [Objectives](#objectives)
+    - [Changes throughout the process](#changes-throughout-the-process)
+2. [Color scheme](#color-scheme)
+3. [Features](#features)
+    - [Welcome page](#welcome-page)
+    - [Instructions](#instructions)
+    - [Game](#game)
+    - [Losing message](#losing-message)   
+    - [Winning message](#winning-message) 
+    - [Colored text](#colored-text) 
+    - [Clear terminal](#clear-terminal)
+    - [Extra features](#extra-features)
+4. [Testing](#testing)
+    - [Python](#python)
+    - [Manual Testing](#manual-testing)
+    - [Bugs](#bugs)
+5. [Deployment](#deployment)
+6. [Finished product](#finished-product)
+7. [Credits](#credits)
--- run.py
+++ run.py
@@ -20,70 +20,94 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+
 def hide_ships():
     # Ship One
     ship_positions = []
     print("Time to position our first ship.")
     ship_one_row = input("Enter ship row between 1-7 \n")
     while ship_one_row not in ["1", "2", "3", "4", "5", "6", "7"]:
-        print("Incorrect input")
+        print("Invalid input, try again")
         ship_one_row = input("Enter ship row between 1-7 \n")
     ship_one_column = input("Enter ship column between A-G \n").upper()
     while ship_one_column not in ["A", "B", "C", "D", "E", "F", "G"]:
-        print("Incorrect input")
+        print("Invalid input, try again")
         ship_one_column = input("Enter ship column between A-G \n").upper()
-    ship_one_position = (int(ship_one_row)-1, letters_to_numbers[ship_one_column])
+    ship_one_position = (
+        int(ship_one_row)-1, letters_to_numbers[ship_one_column]
+    )
     print("Ship one in position")
     ship_positions.append(ship_one_position)
     # Ship Two
     print("Time to position our second ship")
     ship_two_row = input("Enter ship row between 1-7 \n")
     while ship_two_row not in ["1", "2", "3", "4", "5", "6", "7"]:
-        print("Incorrect input")
+        print("Invalid input, try again")
         ship_two_row = input("Enter ship row between 1-7 \n")
     ship_two_column = input("Enter ship column between A-G \n").upper()
     while ship_two_column not in ["A", "B", "C", "D", "E", "F", "G"]:
-        print("Incorrect input.")
+        print("Invalid input, try again.")
         ship_two_column = input("Enter ship column between A-G \n").upper()
-    ship_two_position = (int(ship_two_row)-1, letters_to_numbers[ship_two_column])
+    ship_two_position = (
+        int(ship_two_row)-1, letters_to_numbers[ship_two_column]
+    )
     print("Ship two in position")
     ship_positions.append(ship_two_position)
     # Ship Three
     print("Time to position our third ship")
     ship_three_row = input("Enter ship row between 1-7 \n")
     while ship_three_row not in ["1", "2", "3", "4", "5", "6", "7"]:
-        print("Incorrect input")
+        print("Invalid input, try again")
         ship_three_row = input("Enter ship row between 1-7 \n")
     ship_three_column = input("Enter ship column between A-G \n").upper()
     while ship_three_column not in ["A", "B", "C", "D", "E", "F", "G"]:
-        print("Incorrect input, or there is already a ship there")
+        print("Invalid input, try again")
         ship_three_column = input("Enter ship column between A-G \n").upper()
-    ship_three_position = (int(ship_three_row)-1, letters_to_numbers[ship_three_column])
+    ship_three_position = (
+        int(ship_three_row)-1, letters_to_numbers[ship_three_column]
+    )
     print("Ship three in position")
     ship_positions.append(ship_three_position)
     # Ship Four
     print("Time to position our fourth ship")
     ship_four_row = input("Enter ship row between 1-7 \n")
     while ship_four_row not in ["1", "2", "3", "4", "5", "6", "7"]:
-        print("Incorrect input")
+        print("Invalid input, try again")
         ship_four_row = input("Enter ship row between 1-7 \n")
     ship_four_column = input("Enter ship column between A-G \n").upper()
     while ship_four_column not in ["A", "B", "C", "D", "E", "F", "G"]:
-        print("Incorrect input, or there is already a ship there")
+        print("Invalid input, try again")
         ship_four_column = input("Enter ship column between A-G \n").upper()
-    ship_four_position = (int(ship_four_row)-1, letters_to_numbers[ship_four_column])
+    ship_four_position = (
+        int(ship_four_row)-1, letters_to_numbers[ship_four_column]
+    )
     print("Ship four in position")
     ship_positions.append(ship_four_position)
-    while ship_positions[0] == ship_positions[1] or ship_positions[0] == ship_positions[2] or ship_positions[0] == ship_positions[3]:
-        ship_positions[0] = randint(0,6), randint(0,6)
-    while ship_positions[1] == ship_positions[0] or ship_positions[1] == ship_positions[2] or ship_positions[1] == ship_positions[3]:
-        ship_positions[1] = randint(0,6), randint(0,6)
-    while ship_positions[2] == ship_positions[0] or ship_positions[2] == ship_positions[1] or ship_positions[2] == ship_positions[3]:
-        ship_positions[2] = randint(0,6), randint(0,6)
-    while ship_positions[2] == ship_positions[0] or ship_positions[2] == ship_positions[1] or ship_positions[2] == ship_positions[3]:
-        ship_positions[3] = randint(0,6), randint(0,6)
+    while (
+        ship_positions[0] == ship_positions[1] or
+        ship_positions[0] == ship_positions[2] or
+        ship_positions[0] == ship_positions[3]
+    ):
+        ship_positions[0] = randint(0, 6), randint(0, 6)
+    while (
+        ship_positions[1] == ship_positions[0] or
+        ship_positions[1] == ship_positions[2] or
+        ship_positions[1] == ship_positions[3]
+    ):
+        ship_positions[1] = randint(0, 6), randint(0, 6)
+    while (
+        ship_positions[2] == ship_positions[0] or
+        ship_positions[2] == ship_positions[1] or
+        ship_positions[2] == ship_positions[3]
+    ):
+        ship_positions[2] = randint(0, 6), randint(0, 6)
+    while (
+        ship_positions[3] == ship_positions[0] or
+        ship_positions[3] == ship_positions[1] or
+        ship_positions[3] == ship_positions[3]
+    ):
+        ship_positions[3] = randint(0, 6), randint(0, 6)
     return ship_positions
-    
 
 
 def create_ships(board):
@@ -93,10 +117,12 @@ def create_ships(board):
             ship_row, ship_column = randint(0, 6), randint(0, 6)
         board[ship_row][ship_column] = "X"
 
+
 def get_ship_location():
-    row = input("Enter ship row between 1 and 7:  \n")
-    column = input("Enter ship column between A-G:  \n").upper()
+    row = input("Enter ship row guess between 1 and 7:  \n")
+    column = input("Enter ship column guess between A-G:  \n").upper()
     return int(row)-1, letters_to_numbers[column]
+
 
 def count_hit_ships(board):
     count = 0
@@ -108,15 +134,20 @@ def count_hit_ships(board):
 
 
 create_ships(COMPUTER_BOARD)
-turns = 24
-computer_count = 0
-print("Ahoy admiral, prepare for war! It's time for us to hide our own ships. Pick 4 locations by picking a row and column, and make sure they aren't too easily found. Placing two ships on the same tile is impossible--if you assign two vessels the same location, one will defer to a random location in the ocean.")
+TURNS = 24
+COMPUTER_COUNT = 0
+print(
+    "Ahoy admiral, prepare for war! It's time for us to hide our ships."
+    " Pick 4 locations by picking a row and column. Note that placing two"
+    " ships on the same tile is impossible--if you assign two vessels the same"
+    " location, one will then defer to a random location in the ocean."
+    )
 computer_guess_list = hide_ships()
 print(computer_guess_list)
 print("We have 24 shots for the heavy artillery and reconnaissance tells us there are 4 enemy ships lurking in the area. A blank map of the sea is below, and the gunners are standing by--it's up to you to find the enemy. Good luck.")
 print_board(COMPUTER_BOARD)
 
-while turns > 0:
+while TURNS > 0:
     print_board(GUESS_BOARD)
     row, column = get_ship_location()
     if GUESS_BOARD[row][column] == "-":
@@ -124,31 +155,31 @@ while turns > 0:
     elif COMPUTER_BOARD[row][column] == "X":
         print("A hit! Well shot, sir, ship sunk.")
         GUESS_BOARD[row][column] = "X"
-        computer_guess = randint(0, 6), randint(0,6)
+        computer_guess = randint(0, 6), randint(0, 6)
         if computer_guess in computer_guess_list:
-            computer_count += 1
-            print(f"Some bad news--the enemy sunk one of ours. They've hit {computer_count} of our ships total so far.")
+            COMPUTER_COUNT += 1
+            print(f"Some bad news--the enemy sunk one of ours. They've hit {COMPUTER_COUNT} of our ships total so far.")
         else:
-            print("Some good news--the enemy has missed! Now let's get back on the offensive.")
-        turns -= 1
+            print("Some good news--the enemy has missed!")
+        TURNS -= 1
     else:
         print("No luck sir, shot missed.")
         GUESS_BOARD[row][column] = "-"
-        computer_guess = randint(0, 6), randint(0,6)
+        computer_guess = randint(0, 6), randint(0, 6)
         if computer_guess in computer_guess_list:
-            computer_count += 1
-            print(f"Some bad news--the enemy sunk one of ours. They've hit {computer_count} of our ships total so far.")
+            COMPUTER_COUNT += 1
+            print(f"Some bad news--the enemy sunk one of ours. They've hit {COMPUTER_COUNT} of our ships total so far.")
         else:
-            print("Some good news--the enemy has missed! Now let's get back on the offensive.")
-        turns -= 1
-    
+            print("Some good news--the enemy has missed!")
+        TURNS -= 1
+
     if count_hit_ships(GUESS_BOARD) == 4:
         print("Alas, that's the last of them sir, we've done it! All enemy battleships sunk, we've won!")
         break
-    if computer_count == 4:
+    if COMPUTER_COUNT == 4:
         print("We are finished--thats the last of our navy sinking to the depths. It's been an honor, sir, but the enemy has won.")
         break
-    print(f"We have {turns} shots left. Your updated sea map is below.")
-    if turns == 0:
+    print(f"We have {TURNS} shots left. Your updated sea map is below.")
+    if TURNS == 0:
         print("That's it, we're out of time. We ran out of turns, so both we and the enemy will live to sail another day.")
         break
